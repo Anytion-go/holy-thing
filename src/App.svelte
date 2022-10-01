4e4e63e80603590081f06572a76f3efbe08f7f2a
Nawasan <arikato110011@gmail.com>
2022-10-01 16:56:40 +0700

    add list , ug styles mr

--- src/App.svelte
+++ src/App.svelte
@@ -32,10 +32,30 @@
 
 <main>
     <h1 id="holy" class={saboo}>{getBress}</h1>
-    <h2 class={isFinish ? "" : "go"} on:click={worship}>กด f หรือคลิกเพื่อบูชา</h2>
+    <h2 class={isFinish ? "click" : "click go"} on:click={worship}>
+        กด f หรือคลิกเพื่อบูชา
+    </h2>
+    <div>
+        <a target="_blank" href="https://github.com/Hakata111/holy-thing">
+            <img class="github-icon" src="/github.png" alt="github icon" />
+        </a>
+    </div>
 </main>
 
 <style>
+    .github-icon {
+        margin: 10px;
+        width: 50px;
+        position: fixed;
+        bottom: 0;
+        right: 0;
+    }
+    .click {
+        cursor: pointer;
+    }
+    .click:hover {
+        text-decoration: underline;
+    }
     #holy {
         display: inline-block;
     }
@@ -44,12 +64,12 @@
         animation: gone 7s ease;
     }
     @keyframes gone {
-        0%{
+        0% {
             opacity: 1;
             transform: translate(0, 0);
         }
         50% {
-            opacity: .5;
+            opacity: 0.5;
         }
         100% {
             opacity: 0;
